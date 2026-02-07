ca5a58e004767ad5b6736b461b4782360eeccb1b
Archana Prasad Nair <128218198+Archana-P-Nair@users.noreply.github.com>
2026-02-07 23:04:50 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: "https://captiongenerator-j87m.onrender.com",
+  },
 };
 
 module.exports = nextConfig;
